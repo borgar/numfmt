5154c63e87301a055aa392c2c4c25a55a68751c4
Borgar <borgar@borgar.net>
2022-06-04 20:15:11 +0000

    Use rollup for builds

---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+/* globals process */
+import babel from '@rollup/plugin-babel';
+import { uglify } from 'rollup-plugin-uglify';
+import pkg from './package.json';
+
+const isProd = process.env.NODE_ENV === 'production';
+
+export default {
+  input: pkg.module,
+  plugins: [
+    babel({
+      babelHelpers: 'bundled',
+      presets: [ '@babel/preset-env' ]
+    })
+  ],
+  output: {
+    file: pkg.main,
+    format: 'cjs',
+    sourcemap: true,
+    plugins: [ isProd && uglify() ]
+  }
+};
