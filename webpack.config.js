95175b8ffb78383bafdfda3a6c0038b8a98ab57b
Borgar <borgar@borgar.net>
2022-06-16 09:37:11 +0000

    Version 2.4.1
    Because I had failed to define a compilation target, 2.4 accidentally shifted target with a webpack/babel update. This sets a target and updates the build with one compatible with 2.3.

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,10 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
+          loader: 'babel-loader',
+          options: {
+            presets: [ '@babel/preset-env' ]
+          }
         }
       }
     ]
