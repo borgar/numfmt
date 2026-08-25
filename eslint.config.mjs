684862d110c57e1b7bc9a454a2e4d60e667ce8d4
Borgar <borgar@borgar.net>
2026-08-25 10:59:21 +0000

    Clean up eslint file and disable bugged rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -30,45 +30,10 @@ export default tseslint.config(
   {
     rules: {
       '@stylistic/array-element-newline': 'off',
-      // // next 2 rules are brittle and not really doing much that TS isn't already doing
-      // '@typescript-eslint/no-unnecessary-type-assertion': 'off',
-      // '@typescript-eslint/no-redundant-type-constituents': 'off',
       '@typescript-eslint/no-unsafe-assignment': 'off',
       '@typescript-eslint/no-unsafe-call': 'off',
-      // 'no-use-before-define': 'error',
-      // 'no-labels': 'error',
-      // 'no-restricted-syntax': [ 'error', 'SwitchStatement' ],
-      // 'no-shadow': [ 'error', { builtinGlobals: false } ],
+      // rule is currently bugged
+      'no-useless-assignment': 'off'
     },
   },
 );
-
-/*
-
-{
-  "extends": [ "@borgar/eslint-config", "@borgar/eslint-config/jsdoc" ],
-  "parserOptions": {
-    "ecmaVersion": 2021,
-    "sourceType": "module",
-    "requireConfigFile": false,
-  },
-  "rules": {
-    "import/export": "error",
-    "import/no-unresolved": "error",
-    "no-mixed-operators": "off",
-    "jsdoc/no-undefined-types": "off",
-    "newline-per-chained-call": "off"
-  },
-  "globals": {
-    "BigInt": true,
-    "console": true,
-    "Set": true,
-    "require": true,
-    "module": true,
-  },
-  "plugins": [
-    "import"
-  ]
-}
-
-*/
